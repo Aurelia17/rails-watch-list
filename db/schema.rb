7cc2a81309f52a27c441eef09221df2b16d6c67d
aurelia ritter <aureliaritter@gmail.com>
2023-05-16 16:37:26 +0200

    set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_11_155247) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_115143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_155247) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img_url"
+    t.text "reviews"
   end
 
   create_table "movies", force: :cascade do |t|
